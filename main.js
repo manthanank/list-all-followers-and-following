87a1a4696927bfd18f43165eb0af9b8aaff14e41
Manthan Ank <manthan.ank46@gmail.com>
2022-10-22 16:43:52 +0530

    Update main.js

--- main.js
+++ main.js
@@ -52,14 +52,6 @@ async function run() {
 
     const middle = `
 
-//<img src="${user.avatar_url}" width="120" />
-
-//Username: ${username || '-'} \n
-//Name: ${user.name || '-'}  \n
-//Bio: ${user.bio || '-'} \n
-//Location: ${user.location || '-'} \n
-//Company: ${user.company || '-'} \n
-
 ## Followers <kbd>${followers.length}</kbd>
 
 <table>
